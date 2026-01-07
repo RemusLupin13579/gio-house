0b059f1071c868711753338955f767f8427a7a77
חתוכה <brhvangui12@gmail.com>
2026-01-07 03:20:06 +0200

    Fix: import global CSS in main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import { initAuth } from "./stores/auth";
+import "./assets/main.css";
 
 async function bootstrap() {
     await initAuth();
